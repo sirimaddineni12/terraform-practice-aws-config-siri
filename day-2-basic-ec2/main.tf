2af8f3b84b788fccea76224ffa5bbe3de38cc7d5
sirimaddineni12 <152793298+sirimaddineni12@users.noreply.github.com>
2025-01-20 15:33:03 +0530

    Update main.tf

--- day-2-basic-ec2/main.tf
+++ day-2-basic-ec2/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "dev" {
     ami = var.ami
     instance_type = var.instance_type
     key_name = var.key_name
+    subnet_id = var.subnet_id
     tags = {
       Name = "dev-ec3"
     }
